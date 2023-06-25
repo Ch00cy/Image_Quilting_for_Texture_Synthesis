f4c8560ac31cc36dcf13e852be592b11f539dcb4
ch00cy <chooyeonhee99@gmail.com>
2023-06-25 22:14:42 +0900

    to 4090

--- utils/generate.py
+++ utils/generate.py
@@ -672,7 +672,6 @@ def t_findPatchBoth(refBlockLeft, refBlockTop, img8, img8_mask, blocksize, overl
 			if (img8_mask[r][y:y + blocksize, x:x + blocksize] == 1).all():
 				break
 	elif (tan_mask[(blkIndexI):(blkIndexI + blocksize), (blkIndexJ):(blkIndexJ + blocksize)] == 2).any():  # 마스크 2 안에 들어갈 경우
-		print("22222")
 		errMid = []
 		for ii in range(len(errMat)):
 			for jj in range(len(where_mid)):
@@ -834,7 +833,7 @@ def foam_generateTextureMap(image, blocksize, overlap, outH, outW, tolerance):	#
 	c, d = a//2, b//2
 	tan_mask = np.zeros((a,b))
 
-	angle = 135	# 주어진 각도 - 회전된 직선 영역을 위하여
+	angle = 20	# 주어진 각도 - 회전된 직선 영역을 위하여
 	slope = 0	# 회전된 직선영역의 기울기
 	is_90 = False	# flag : 90도인가, 90도일경우에만 직선의 방정식 x= a 꼴이기 때문
 
